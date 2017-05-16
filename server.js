1b4ac04fd922dff2489a114b45b544731f05a0d1
Tracey Trewin <tracey@trewin.com>
2017-05-16 08:46:59 -0700

    added app insights

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
+// setup for app insights
+const appInsights = require("applicationinsights");
+appInsights.setup("0dccd139-e558-41b0-838c-fd892a3b0b16");
+appInsights.start();
+
 var express = require('express')
 var app = express();
 
 const PORT = 5000;
 
 app.get('/', function(req, res) {
-    res.send('Server has started again!')
+    res.send('Server has started again!');
 });
 
 app.get('/getTest', function(req, res) {
-    res.send('testing getTest')
+    appInsights.client.trackRequest(req, res);
+    res.send('testing getTest again and again');
 });
 
 app.listen(PORT);
